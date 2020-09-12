7d200acd27379077dc1cd8f428dfd75bda54a0c3
Hossan Rose <me@hossanrose.com>
2020-09-12 16:27:59 +0100

    Metrics endpoint

--- app/app.py
+++ app/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 from markupsafe import escape
+from prometheus_flask_exporter import PrometheusMetrics
 import time
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+@app.route('/')
+def main():
+    return 'OK'
 
 @app.route('/hello')
 def hello_world():
     return 'Hello world | Time: %s' % escape(time.strftime('%B, %d %Y %H:%M:%S'))
+
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
